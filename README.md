de0626d31fbb0e6f822cc0026dff32555e0dd323
Chirayu Desai <chirayudesai1@gmail.com>
2012-08-12 14:44:32 +0530

    yaay moar stuff

--- README.md
+++ README.md
@@ -5,7 +5,10 @@ Some stuff I use.
 
 bin/
 
-    Some binaries, compiled from AOSP Master, useful for Android Developers
+    Some binaries and scripts.
+    A few binaries compiled from AOSP master, a few obtained from teh internetz.
+    A few scripts i made, and a few from teh internetz.
+    Useful for Android Developers :)
     Linux-x86
 
 More to come! ;)
---
+++ bin/rd_aosp.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import sys
+import urllib
+import urllib2
+import json
+import os
+
+for change in sys.argv[1:]:
+    print change
+    f = urllib2.urlopen('https://android-review-googlesource.com/query?q=change:%s' % change)
+    d = f.read()
+    # gerrit doesnt actually return json. returns two json blobs, separate lines. bizarre.
+    print d
+    d = d.split('\n')[0]
+    data = json.loads(d)
+    project = data['project']
+    project = project.replace('platform/', '')
+
+    while not os.path.isdir(project):
+        new_project = project.replace('_', '/', 1)
+        if new_project == project:
+            break
+        project = new_project
+
+    print project
+    number = data['number']
+    patch_count = 0
+    junk = number[len(number) - 2:]
+
+    while 0 != os.system('cd %s ; git fetch https://android-review-googlesource.com/%s refs/changes/%s/%s/%s' % (project, data['project'], junk, number, patch_count + 1)):
+        patch_count = patch_count + 1
+
+    while 0 == os.system('cd %s ; git fetch https://android-review-googlesource.com/%s refs/changes/%s/%s/%s' % (project, data['project'], junk, number, patch_count + 1)):
+        patch_count = patch_count + 1
+
+    os.system('cd %s ; git fetch https://android-review-googlesource.com/%s refs/changes/%s/%s/%s' % (project, data['project'], junk, number, patch_count))
+    os.system('cd %s ; git merge FETCH_HEAD' % project)
+
